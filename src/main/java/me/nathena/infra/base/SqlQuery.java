e288d03267cd952cf6a0048db7e62e6d2c2e36e8
GaoWx <297200625@qq.com>
2015-08-13 20:18:03 +0800

    增加between in和like的搜索条件

--- src/main/java/me/nathena/infra/base/SqlQuery.java
+++ src/main/java/me/nathena/infra/base/SqlQuery.java
@@ -21,6 +21,7 @@ public class SqlQuery {
 	private String column; 
 	private String opr;
 	private Object value;
+	private Object value2;
 	private String connect = "AND";
 	public SqlQuery() {}
 	public SqlQuery(String connect, String column, String opr, Object values) {
@@ -34,6 +35,20 @@ public class SqlQuery {
 		this.column = column;
 		this.opr = opr;
 		this.value = values;
+	}
+	//双目运算符
+	public SqlQuery(String column, String opr, Object values, Object values2) {
+		this.column = column;
+		this.opr = opr;
+		this.value = values;
+		this.value2 = values2;
+	}
+	public SqlQuery(String connect, String column, String opr, Object values, Object values2) {
+		this.column = column;
+		this.opr = opr;
+		this.connect = connect;
+		this.value = values;
+		this.value2 = values2;
 	}
 	
 	public String getColumn() {
@@ -60,8 +75,14 @@ public class SqlQuery {
 	public void setConnect(String connect) {
 		this.connect = connect;
 	}
+	public Object getValue2() {
+		return value2;
+	}
+	public void setValue2(Object value2) {
+		this.value2 = value2;
+	}
 	public static final String eq = "=", gt = ">", gte = ">=", lt = "<", lte = "<=",
-							   in = "IN", between_and = "bt";
+							   in = "IN", between_and = "bt", like = "LIKE", like_left = "LIKE_LEFT", like_right = "LIKE_RIGHT";
 	public String toSearchSql(Map<String, Object> paramMap) {
 		if(StringUtils.isEmpty(opr)) {
 			return "";
@@ -73,10 +94,18 @@ public class SqlQuery {
 			paramMap.put(column + r, value);
 			return new StringBuffer(" ").append(connect).append(" ").append(column).append(" ").append(opr).append(" (:").append(column).append(r).append(")").toString();
 		case between_and:
-//			paramMap.put(column + r + "1", value);
-//			paramMap.put(column + r + "2", value);
-			//TODO
+			paramMap.put(column + r + "1", value);
+			paramMap.put(column + r + "2", value2);
 			return new StringBuffer(" ").append(connect).append(" ").append(column).append(" BETWEEN :").append(column).append(r).append("1").append(" AND :").append(column).append(r).append("2").toString();
+		case like:
+			paramMap.put(column + r, "%" + value + "%");
+			return new StringBuffer(" ").append(connect).append(" ").append(column).append(" LIKE :").append(column).append(r).toString();
+		case like_left:
+			paramMap.put(column + r, "%" + value);
+			return new StringBuffer(" ").append(connect).append(" ").append(column).append(" LIKE :").append(column).append(r).toString();
+		case like_right:
+			paramMap.put(column + r, value + "%");
+			return new StringBuffer(" ").append(connect).append(" ").append(column).append(" LIKE :").append(column).append(r).toString();
 		default:
 			paramMap.put(column + r, value);
 			return new StringBuffer(" ").append(connect).append(" ").append(column).append(" ").append(opr).append(" :").append(column).append(r).toString();
