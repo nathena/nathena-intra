c8b42810f0d129eeb454e9fa202f9a626f00f2eb
GaoWx <297200625@qq.com>
2015-11-17 16:50:52 +0800

    url安全的base64编码

--- src/main/java/me/nathena/infra/crypto/Base64Coder.java
+++ src/main/java/me/nathena/infra/crypto/Base64Coder.java
@@ -28,5 +28,19 @@ public final class Base64Coder {
     {
     	return Base64.encodeBase64String(data.getBytes("UTF-8"));
     }
-
+    
+    //URL安全的编码方式+换成-,/换成_去掉最后的=
+    public static String uriSafeEncode(byte[] data)
+    {
+    	String encodeStr = Base64.encodeBase64String(data);
+    	String uriSafeStr = encodeStr.replaceAll("\\+", "-").replaceAll("/", "_").replaceAll("=", "");
+    	return uriSafeStr.substring(0, uriSafeStr.length());
+    }
+    //URL安全的解码方式
+    public static byte[] uriSafeDncode(byte[] data)
+    {
+    	String decodeStr = new String(data);
+    	decodeStr = decodeStr.replaceAll("-", "\\+").replaceAll("_", "/");
+    	return Base64.decodeBase64(decodeStr.getBytes());
+    }
 }
