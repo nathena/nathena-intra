7d2250e314b93d5178063da5fe5916a18dfc8ca1
nathena@qq.com <nathena@qq.com>
2016-04-08 19:32:09 +0800

    加强md5算法

--- src/main/java/me/nathena/infra/crypto/MD5Coder.java
+++ src/main/java/me/nathena/infra/crypto/MD5Coder.java
@@ -22,6 +22,25 @@ public final class MD5Coder {
 		return sb.toString();
 	} 
 	
+	public static String encode(byte[] src,byte[] key)
+	{
+		StringBuilder sb=new StringBuilder();
+		try 
+		{
+			java.security.MessageDigest md=java.security.MessageDigest.getInstance("MD5");
+			md.update(src);
+			for(byte b:md.digest(key))
+			{
+				sb.append(Integer.toString(b>>>4&0xF,16)).append(Integer.toString(b&0xF,16));
+			}
+		} 
+		catch (Exception e) 
+		{
+			throw new RuntimeException(e.getMessage(), e);
+		}
+		return sb.toString();
+	} 
+	
 	public static String encode(String src)
 	{
 		try
@@ -38,8 +57,24 @@ public final class MD5Coder {
 		}
 	}
 	
+	public static String encode(String src,String key)
+	{
+		try
+		{
+			if(src==null)
+			{
+				return "";
+			}
+			return encode(src.getBytes("UTF-8"),key.getBytes("UTF-8"));
+		}
+		catch(Exception e)
+		{
+			throw new RuntimeException(e.getMessage(), e);
+		}
+	}
+	
 	public static void main(String[] arg)
 	{
-		System.out.println(encode("Jixi.app@Jytnn.com123"));
+		System.out.println(encode("12345678","19pay.com.cn"));
 	}
 }
